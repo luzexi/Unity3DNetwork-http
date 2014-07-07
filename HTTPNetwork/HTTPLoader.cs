8c763d6b6727eac0c04b1f338ae7503779cf1ebd
luzexi <zexi.lu@gumichina.com>
2014-07-07 16:48:44 +0800

    第二轮测试修复
    增加错误回调以及回调后重新发送接口。

--- HTTPNetwork/HTTPLoader.cs
+++ HTTPNetwork/HTTPLoader.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
+using SimpleJSON;
+using System;
 
 
 //  HTTPLoader.cs
@@ -13,46 +15,111 @@ namespace Game.Network
     /// <summary>
     /// HTTP加载
     /// </summary>
-    public class HTTPLoader : MonoBehaviour
+	public class HTTPLoader : MonoBehaviour 
     {
-		public delegate void CALLBACK<T>(WWW www , System.Action<T> callback);
-		//public delegate void CALLBACK2<T>(System.Action<T> callback);
+		private STATE m_eState;
+		private enum STATE
+		{
+			START = 0,
+			STOP,
+			CLOSE
+		}
 
         /// <summary>
-        /// 开始WWW
+        /// Gos the WWW.
         /// </summary>
-        /// <param name="url"></param>
-        /// <param name="callback"></param>
-        /// <returns></returns>
-		public static void GoWWW<T>(string url,WWWForm form , CALLBACK<T> callback , System.Action<T> callback2 )
+        /// <returns>The WW.</returns>
+        /// <param name="url">URL.</param>
+        /// <param name="form">Form.</param>
+        /// <param name="error_callback">Error_callback.</param>
+        /// <param name="callback2">Callback2.</param>
+		public static void GoWWW<T>(
+			string url , WWWForm form ,
+			System.Action<string,System.Action,System.Action> error_callback ,
+			System.Action<T> callback2
+			)
+			where T : HTTPPacketAck
         {
             GameObject obj = new GameObject("HTTPLoader");
             HTTPLoader loader = obj.AddComponent<HTTPLoader>();
-            loader.StartCoroutine(loader.StartHTTP<T>(url , form , callback , callback2));
+			loader.m_eState = STATE.START;
+            loader.StartCoroutine(loader.StartHTTP<T>(url , form , error_callback , callback2));
         }
 
+		/// <summary>
+		/// Res the send.
+		/// </summary>
+		public void ReSend()
+		{
+			this.m_eState = STATE.START;
+		}
+
+		/// <summary>
+		/// Close this instance.
+		/// </summary>
+		public void Close()
+		{
+			this.m_eState = STATE.CLOSE;
+		}
+
         /// <summary>
-        /// 开始HTTP
+        /// Starts the HTTP.
         /// </summary>
-        /// <param name="url"></param>
-        /// <param name="callback"></param>
-        /// <returns></returns>
-        public IEnumerator StartHTTP<T>(string url , WWWForm form, CALLBACK<T> callback , System.Action<T> callback2 )
+        /// <returns>The HTT.</returns>
+		public IEnumerator StartHTTP<T>(string url , WWWForm form ,
+		                                System.Action<string,System.Action,System.Action> error_callback ,
+		                                System.Action<T> callback2)
+			where T : HTTPPacketAck
         {
-            Debug.Log("send url " + url);
-            url = System.Uri.EscapeUriString(url);
 			WWW www = null;
-			if( form != null )
-				www = new WWW(url,form);
-			else
-				www = new WWW(url);
-
-            yield return www;
-
-            if (callback != null)
-                callback(www , callback2);
-
-            GameObject.Destroy(this.gameObject);
+			bool ok = true;
+			for( ; ok ;)
+			{
+				switch(this.m_eState)
+				{
+				case STATE.START:
+					Debug.Log("send url " + url);
+					url = System.Uri.EscapeUriString(url);
+					if( form != null )
+						www = new WWW(url , form);
+					else
+						www = new WWW(url);
+					yield return www;
+					try
+					{
+						if (www.error != null)
+						{
+							Debug.LogError("ERROR HTTP : " + www.error);
+							this.m_eState = STATE.STOP;
+							if(error_callback != null)
+								error_callback(www.error,ReSend , Close);
+						}
+						else
+						{
+							Debug.Log(www.text);
+							JSONNode obj = JSON.Parse(www.text);
+							Debug.Log(obj.ToString());
+							T packet = JsonPacker.Unpack(obj , typeof(T)) as T;
+							this.m_eState = STATE.CLOSE;
+							callback2(packet);
+						}
+					}
+					catch( Exception ex )
+					{
+						this.m_eState = STATE.STOP;
+						if(error_callback != null)
+							error_callback(ex.StackTrace,ReSend , Close);
+					}
+					break;
+				case STATE.STOP:
+					yield return new WaitForFixedUpdate();
+					break;
+				case STATE.CLOSE:
+					GameObject.Destroy(this.gameObject);
+					ok = false;
+					break;
+				}
+			}
         }
 
     }
--- HTTPNetwork/HTTPSession.cs
+++ HTTPNetwork/HTTPSession.cs
@@ -1,12 +1,11 @@
 ﻿using System;
+using System.Reflection;
 using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
 using SimpleJSON;
 
-using HTTP_ON_DATAERROR = System.Action<string>;
-using HTTP_ON_TIMEOUT = System.Action;
-using HTTP_ON_DISCONNECT = System.Action;
+using HTTP_ON_DATAERROR = System.Action<string,System.Action,System.Action>;
 
 
 //  HttpSession.cs
@@ -25,47 +24,11 @@ namespace Game.Network
         private string m_strURL = "";   //主地址
 
 		public HTTP_ON_DATAERROR onDataError = null;
-		public HTTP_ON_TIMEOUT onTimeOut = null;
-		public HTTP_ON_DISCONNECT onDisconnect = null;
 
         public HTTPSession(string url)
         {
             this.m_strURL = url;
         }
-
-		/// <summary>
-		/// Raises the data error event.
-		/// </summary>
-		/// <param name="error">Error.</param>
-		private void OnDataError( string error )
-		{
-			if(this.onDataError != null )
-			{
-				this.onDataError(error);
-			}
-		}
-
-		/// <summary>
-		/// Raises the time out event.
-		/// </summary>
-		private void OnTimeOut()
-		{
-			if(this.onTimeOut != null )
-			{
-				this.onTimeOut();
-			}
-		}
-
-		/// <summary>
-		/// Raises the disconnect event.
-		/// </summary>
-		private void OnDisconnect()
-		{
-			if(this.onDisconnect != null )
-			{
-				this.onDisconnect();
-			}
-		}
 
         /// <summary>
         /// Sends the GET data.
@@ -75,7 +38,7 @@ namespace Game.Network
         /// <typeparam name="T">The 1st type parameter.</typeparam>
         public void SendGET<T>(HTTPPacketRequest packet ,System.Action<T> callback) where T : HTTPPacketAck
         {
-			HTTPLoader.GoWWW<T>(this.m_strURL + packet.GetAction() + packet.ToParam() , null , FinishCallBack , callback);
+			HTTPLoader.GoWWW<T>(this.m_strURL + packet.GetAction() + packet.ToParam() , null , onDataError , callback);
         }
 
 		/// <summary>
@@ -86,38 +49,38 @@ namespace Game.Network
 		/// <typeparam name="T">The 1st type parameter.</typeparam>
 		public void SendPOST<T>(HTTPPacketRequest packet ,System.Action<T> callback) where T : HTTPPacketAck
 		{
-			HTTPLoader.GoWWW<T>(this.m_strURL + packet.GetAction(), packet.ToForm() , FinishCallBack , callback);
+			HTTPLoader.GoWWW<T>(this.m_strURL + packet.GetAction(), packet.ToForm() , onDataError , callback);
 		}
 
-		/// <summary>
-		/// Calls the back.
-		/// </summary>
-		/// <param name="www">Www.</param>
-		/// <typeparam name="T">The 1st type parameter.</typeparam>
-		private void FinishCallBack<T>(WWW www , System.Action<T> callback) where T : HTTPPacketAck
-		{
-			try
-			{
-				if (www.error != null)
-				{
-					Debug.LogError("ERROR HTTP : " + www.error);
-					OnDisconnect();
-				}
-				else
-				{
-					Debug.Log(www.text);
-					JSONNode obj = JSON.Parse(www.text);
-					Debug.Log(obj.ToString());
-					T packet = JsonPacker.Unpack(obj , typeof(T)) as T;
-					callback(packet);
-				}
-			}
-			catch (Exception ex)
-			{
-				Debug.LogError(ex.StackTrace);
-				OnDataError(ex.StackTrace);
-			}
-		}
+//		/// <summary>
+//		/// Calls the back.
+//		/// </summary>
+//		/// <param name="www">Www.</param>
+//		/// <typeparam name="T">The 1st type parameter.</typeparam>
+//		private void FinishCallBack<T>(WWW www , System.Action<T> callback) where T : HTTPPacketAck
+//		{
+//			try
+//			{
+//				if (www.error != null)
+//				{
+//					Debug.LogError("ERROR HTTP : " + www.error);
+//					OnDisconnect();
+//				}
+//				else
+//				{
+//					Debug.Log(www.text);
+//					JSONNode obj = JSON.Parse(www.text);
+//					Debug.Log(obj.ToString());
+//					T packet = JsonPacker.Unpack(obj , typeof(T)) as T;
+//					callback(packet);
+//				}
+//			}
+//			catch (Exception ex)
+//			{
+//				Debug.LogError(ex.StackTrace);
+//				OnDataError(ex.StackTrace);
+//			}
+//		}
     }
 
 
